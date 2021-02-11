f0e0f3b6310890ea0cffcbda86947d5c2408848c
Edgar Castro <edgar_castro@g.harvard.edu>
2021-02-11 03:50:21 -0500

    data.table for i/o, concats, joins; re-enabled wide outputs; compressed outputs; no user interaction

--- measures.R
+++ measures.R
@@ -1,44 +1,51 @@
+library(data.table)
 library(dplyr)
-library(purrr)
 library(readxl)
 library(stringr)
 library(totalcensus)
 
 #states <- states_DC
 states <- c("MA")
+
 output_directory <- "generated"
 
-# Note that this function requires user interaction
-#set_path_to_census("/media/qnap3/Covariates/totalcensus/")
+years <- c(2009:2019)
+
+geometries <- c("tract", "block group")
 
 # Originally located in my home directory due to the very long download not
 # playing well with the stability of qnap3; later moved to qnap3. See the setup
 # section below.
-#set_path_to_census("/home/edgar/totalcensus/")
+#totalcensus_path <- "/media/qnap3/Covariates/totalcensus/"
+#totalcensus_path <- "/home/edgar/totalcensus/"
+totalcensus_path <- "./totalcensus/"
 
-set_path_to_census("./totalcensus/")
 
 # Setup --------------------------------------------------------------------
 
-dir.create(output_directory)
+dir.create(output_directory, showWarnings = FALSE)
+
+# Official docs of `totalcensus` say to use `set_path_to_census` but that
+# function requires user interaction
+Sys.setenv(PATH_TO_CENSUS = totalcensus_path)
 
 # Pre-download all the data so we don't get prompted during the main loop
-
+#
 # The network drives sometimes have some IO issues due to the size of this
 # (~50GB/year) so it's better to download everything to a home directory and
 # then copy the processed files over to the QNAPs later
-
+#
 # Note that this function requires user interaction
-for (year in c(2009:2019)) {
-  download_census("acs5", year, states = states)
-}
+#for (year in years) {
+#  download_census("acs5", year, states = states)
+#}
 
 # Start loops ---------------------------------------------------------------
 # Don't run this section if you are working with a specific set of constants
 # from above
 
-for (year in c(2009:2019)) {
-  for (geometry in c("tract", "block group")) {
+for (year in years) {
+  for (geometry in geometries) {
     cat(sprintf("Processing: %s %ss\nCalculating:\n", year, geometry))
 
 # Plain Census values -----------------------------------------------------
@@ -187,7 +194,7 @@ cat(" ok\n")
 
 output_file <- file.path(
   output_directory,
-  sprintf("%d_%ss.csv", year, str_replace(geometry, " ", "_"))
+  sprintf("%d_%ss.csv.gz", year, str_replace(geometry, " ", "_"))
 )
 cat(sprintf("Joining and writing to %s...", output_file))
 
@@ -200,7 +207,7 @@ Reduce(
     ice_race_ethnicity
   )
 ) %>%
-    write.csv(output_file, row.names = FALSE)
+    fwrite(output_file)
 
 cat(" ok\n")
 
@@ -214,59 +221,84 @@ cat(" ok\n")
 # Merge yearly files into one ---------------------------------------------
 
 # Wide
-#Reduce(
-#  function(x, y) left_join(x, y, by = "GEOID"),
-#  lapply(
-#    c(2010:2019),
-#    function(year) read.csv(
-#      file.path(
-#        output_directory,
-#        sprintf("%d_%ss.csv", year, str_replace(geometry, " ", "_"))
-#      )
-#    ) %>%
-#      rename_all(paste0, "_", year) %>%
-#      mutate(GEOID = str_extract(get(paste0("GEOID_", year)), "US[0-9]+"))
-#  )
-#) %>%
-#  write.csv(
-#    file.path(
-#      output_directory,
-#      sprintf(
-#        "time_series_2010_to_2019_%ss.csv",
-#        sprintf("%d_%ss.csv", year, str_replace(geometry, " ", "_"))
-#      )
-#    ),
-#    row.names = FALSE
-#  )
-
-# Long
-cat("Merging into long format:")
-for (geometry in c("tract", "block group")) {
+cat("Merging into wide format:")
+for (geometry in geometries) {
   cat(sprintf("* %ss:", geometry))
-  data.table::rbindlist(
+  
+  merged <- Reduce(
+    #function(x, y) left_join(x, y, by = "GEOID"), # dplyr
+    function(x, y) x[y, on = "GEOID"], # data.table
     lapply(
-      c(2010:2019),
+      years,
       function(year) {
         cat(sprintf(" %s...", year))
-        read.csv(
+        fread(
           file.path(
             output_directory,
-            sprintf("%d_%ss.csv", year, str_replace(geometry, " ", "_"))
+            sprintf("%d_%ss.csv.gz", year, str_replace(geometry, " ", "_"))
+          )
+        ) %>%
+          rename_all(paste0, "_", year) %>%
+          mutate(GEOID = str_extract(get(paste0("GEOID_", year)), "US[0-9]+"))
+      }
+    )
+  )
+  
+  cat(" writing...")
+  fwrite(
+    merged,
+    file.path(
+      output_directory,
+      sprintf(
+        "time_series_%s_to_%s_%ss_wide.csv.gz",
+        min(years),
+        max(years),
+        str_replace(geometry, " ", "_")
+      )
+    )
+  )
+  
+  cat(" ok\n")
+  rm(merged)
+}
+
+# Long
+cat("Writing into long format:")
+for (geometry in geometries) {
+  cat(sprintf("* %ss:", geometry))
+  
+  # can't figure out how to append to .csv.gz files in R so we just concatenate
+  # everything before writing it out
+  concatenated <- rbindlist(
+    lapply(
+      years,
+      function(year) {
+        cat(sprintf(" %s...", year))
+        fread(
+          file.path(
+            output_directory,
+            sprintf("%d_%ss.csv.gz", year, str_replace(geometry, " ", "_"))
           )
         ) %>%
           mutate(year = year)
       }
     )
-  ) %>%
-    write.csv(
-      file.path(
-        output_directory,
-        sprintf(
-          "time_series_2010_to_2019_%ss.csv",
-          str_replace(geometry, " ", "_")
-        )
-      ),
-      row.names = FALSE
+  )
+    
+  cat(" writing...")
+  fwrite(
+    concatenated,
+    file.path(
+      output_directory,
+      sprintf(
+        "time_series_%s_to_%s_%ss_long.csv.gz",
+        min(years),
+        max(years),
+        str_replace(geometry, " ", "_")
+      )
     )
+  )
+
   cat(" ok\n")
+  rm(concatenated)
 }
