a5d73bb36fdcbd5a2c7b9795c5c511c16f2e1876
Edgar Castro <edgar_castro@g.harvard.edu>
2021-02-20 05:24:51 -0500

    added test for validating shapefiles against tigris

--- validation.R
+++ validation.R
@@ -3,19 +3,27 @@ library(sf)
 library(stringr)
 library(testthat)
 
+# Some tests will make requests to websites to retrieve external data for
+# validation - set this to FALSE if that is not desired
+allow_internet <- TRUE
+
 # Setup -------------------------------------------------------------------
 
 # Note that we will be using Massachusetts data
+cat("Reading data...\n")
 measures <- read.csv("outputs/tables/ma/time_series_2009_to_2019_tracts_long.csv.gz")
 crosswalk <- read.csv("outputs/crosswalks/ma_tracts_crosswalk.csv.gz")
 shapefile <- st_read("outputs/crosswalks/ma_tracts.shp", quiet = TRUE)
 
-merged <- left_join(
+cat("Merging data...\n")
+merged_no_measures <- left_join(
     crosswalk,
     mutate(shapefile, GEOID = as.numeric(GEOID)),
     by = c("GEOID", "amended")
   ) %>%
-  left_join(
+  st_as_sf(crs = st_crs(shapefile))
+merged <- left_join(
+    merged_no_measures,
     mutate(measures, GEOID = as.numeric(str_sub(GEOID, 8))),
     by = c("GEOID", "year")
   ) %>%
@@ -26,6 +34,8 @@ percent_difference <- function(actual, expected) {
 }
 
 # Tests -------------------------------------------------------------------
+
+cat("Running tests...\n")
 
 test_that("Correct number of tracts per year", {
   tracts_2010_to_2019 <- rep(1478, 2019-2010+1)
@@ -38,10 +48,10 @@ test_that("Correct number of tracts per year", {
   expect_equal(as.numeric(table(measures$year)), c(1366, tracts_2010_to_2019))
 })
 
-test_that("Similar overall population density", {
+test_that("Similar overall population density (2010)", {
   # There will be some discrepancy due to use of ACS 5-year estimates rather
   # than 2010 Decennial Census data
-  tolerance <- 0.05
+  tolerance <- 0.05 # percent difference
   
   # Values from from US Census Guide to 2010 State and Local Census Geography
   actual_area <- 7800.1 # square miles
@@ -61,3 +71,43 @@ test_that("Similar overall population density", {
     tolerance
   )
 })
+
+# Will rely on the `tigris` library for external validation here
+if (allow_internet) {
+  test_that("Matching GEOIDs and similar areas (2010-2019)", {
+    tolerance <- 0.01 # percent difference
+    
+    # Having issues getting 2009 tracts via tigris so only testing 2010-2019
+    for (year_ in c(2010:2019)) { 
+      geoid_column <- "GEOID"
+      if (year_ == 2010) {
+        geoid_column <- "GEOID10"
+      }
+      
+      tigris_tracts <- tigris::tracts("MA", year = year_) %>%
+        mutate(GEOID = as.numeric(!!as.name(geoid_column))) %>%
+        arrange(GEOID)
+      
+      tigris_areas <- st_area(tigris_tracts)
+      
+      crosswalk_tracts <- merged_no_measures %>%
+        filter(year == year_) %>%
+        arrange(GEOID)
+      
+      crosswalk_areas <- st_area(crosswalk_tracts)
+      
+      expect_equal(nrow(tigris_tracts), nrow(crosswalk_tracts))
+      
+      expect_equal(tigris_tracts$GEOID, crosswalk_tracts$GEOID)
+      
+      # Test that areas are similar
+      expect_equal(
+        percent_difference(tigris_areas, crosswalk_areas),
+        rep(0, nrow(tigris_tracts)),
+        tol = tolerance
+      )
+    }
+  })
+} else{
+  cat("Skipped test that needed Internet access")
+}
